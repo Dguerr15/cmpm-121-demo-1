3d53a47e1e1bed70b23b485a751e2b0db4b8dc5e
Dguerr15 <dguerr15@ucsc.edu>
2024-10-08 20:12:20 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Davids amazingly fun game";
+const gameName = "Dino Wrangler";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🦕";
+button.onclick = () => {
+    console.log("working!");
+  };
+app.append(button);
